dddcb87f8882aab447f32593364f93a7e2319f93
Melanie Cisler <65982774+melaniecisler@users.noreply.github.com>
2020-12-04 18:20:57 -0600

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const Workout = require("../models").Workout;
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(workouts => res.json(workouts))
+    .catch(err => res.json(err));
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then(workout => res.json(workout))
+    .catch(err => res.json(err));
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({
+    day: Date.now()
+  })
+    .then(newWorkout => {
+      console.log("this is the created workout: ", newWorkout);
+      res.json(newWorkout);
+    })
+    .catch(err => res.json(err));
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then(workouts => {
+      res.json(workouts);
+    })
+    .catch(err => res.json(err));
+});
+
+router.delete("/api/workouts", (req, res) => {
+  //  delete route 
+});
+
+module.exports = router;
